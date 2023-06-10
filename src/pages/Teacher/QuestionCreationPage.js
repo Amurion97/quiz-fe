5dffcefdbab801fa57a5ba00c87d9a07ca2fe110
Van-Giang NGUYEN <118123724+Amurion97@users.noreply.github.com>
2023-06-10 20:30:19 +0700

    trimming

---
+++ src/pages/Teacher/QuestionCreationPage.js
@@ -0,0 +1,35 @@
+import {Helmet} from 'react-helmet-async';
+// @mui
+import {Grid} from '@mui/material';
+// hooks
+// components
+// sections
+import FlightCreationForm from "../../components/Forms/FlightCreationForm";
+
+// ----------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+
+export default function QuestionCreationPage() {
+    return (
+        <>
+            <Helmet>
+                <title> Flight Creation | Flight </title>
+            </Helmet>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                style={{
+                    height: "100%",
+                    padding: "5%"
+                }}
+            >
+                <Grid item xs={12}>
+                    <QuestionCreationF/>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
