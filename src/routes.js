f27f155febfc4a5e7b4fd2ad3f7931cbed85c6b1
Van-Giang NGUYEN <118123724+Amurion97@users.noreply.github.com>
2023-06-16 13:57:10 +0700

    router modified

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,7 @@ import {Navigate, useRoutes} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import Page404 from './pages/Page404';
 import DashboardLayout from "./layouts/DashboardLayout";
-import FlightCreationPage from "./pages/User/FlightCreationPage";
 import UsersPage from "./pages/AdminPrivilege/UsersPage";
-import SearchPage from "./pages/Booking/SearchPage";
-import ResultsPage from "./pages/Booking/ResultsPage";
-import BookingFinalization from "./pages/Booking/BookingFinalization";
-import AircraftPage from "./pages/User/AircraftPage";
-import FlightPage from "./pages/User/FlightPage";
-import AirportPage from "./pages/User/AirportPage";
-import BookingDetailsPage from "./pages/Booking/BookingDetailsPage";
 import {useSelector} from 'react-redux';
 import QuestionCreationPage from "./pages/Teacher/QuestionCreationPage";
 import StudentLayout from "./layouts/StudentLayout";
@@ -21,18 +13,17 @@ import RegisterPage from "./pages/RegisterPage";
 import QuestionSearchResults from "./pages/Teacher/QuestionSearchResults";
 import QuestionManagement from "./pages/Teacher/QuestionManagement";
 import QuestionEditPage from "./pages/Teacher/QuestionEditPage";
+import {selectUser} from "./features/user/userSlice";
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
-    let user = useSelector(({user}) => {
-        return user;
-    });
+    let user = useSelector(selectUser);
     const routes = useRoutes([
         {
             path: '/dashboard',
-            element: <DashboardLayout/>,
-            children: [
+            element: user.info ? <DashboardLayout/> : <Navigate to={'/login'}/>,
+            children: user.info ? [
                 {element: <Navigate to="/dashboard/questions"/>, index: true},
                 {path: 'users', element: (user.info ? (user.info.role === 1 ? <UsersPage/> : <Page404/>) : <Page404/>)},
 
@@ -44,7 +35,7 @@ export default function Router() {
 
                 {path: '404', element: <Page404/>},
                 {path: '*', element: <Navigate to="/dashboard/404"/>},
-            ],
+            ] : [],
         },
         {
             path: '/students',
@@ -80,7 +71,9 @@ export default function Router() {
         },
         {
             path: "/",
-            element: <Navigate to="/login"/>,
+            // element: <Navigate to="/login"/>,
+            element: (user.info ? (user.info.role <= 2 ? <Navigate to="/dashboard"/> :
+                <Navigate to="/students/listData"/>) : <Navigate to="/login"/>),
         },
 
 
