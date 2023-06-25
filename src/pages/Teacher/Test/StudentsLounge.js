65e364738fa2c7e0139a95258514d615270659b9
Van-Giang NGUYEN <118123724+Amurion97@users.noreply.github.com>
2023-06-25 22:56:10 +0700

    feature/student/22.1 HomePage code enter Input

---
+++ src/pages/Teacher/Test/StudentsLounge.js
@@ -0,0 +1,23 @@
+import {Avatar, Box, Card, Grid} from "@mui/material";
+import CardHeader from "@mui/material/CardHeader";
+
+export default function StudentsLounge() {
+    return (
+        <>
+            <Grid container spacing={3}>
+                <Grid item xs={12 / 4}>
+                    <Box>
+                        <Card sx={{pb: "24px", height: "100%"}}>
+                            <CardHeader
+                                avatar={<Avatar src="/assets/images/avatars/avatar_default.jpg"
+                                                alt="photoURL"/>}
+                                title="User"
+                                subheader="Friend"
+                            />
+                        </Card>
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
