87d4aa9aff224d3b91636ca867180f6d13bfa697
Van-Giang NGUYEN <118123724+Amurion97@users.noreply.github.com>
2023-06-27 17:13:38 +0700

    Updates

---
+++ static/js/364.3e6119d7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfe=self.webpackChunkfe||[]).push([[364],{9364:function(n,e,r){r.r(e),r.d(e,{default:function(){return f}});var t=r(2791),s=r(4306),u=r(2513),c=r(184),i=function(){return(0,c.jsxs)(s.YI,{children:[(0,c.jsx)(s.WD,{name:"App",children:(0,c.jsx)(s.wA,{name:"Loader",children:(0,c.jsx)(s.$e,{children:(0,c.jsx)(a,{})})})}),(0,c.jsx)(u.Z,{})]})};function a(){return(0,c.jsx)(t.Fragment,{children:"Loading..."})}var f=function(){return(0,c.jsx)(s.Vt,{palette:(0,c.jsx)(i,{})})}}}]);
+//# sourceMappingURL=364.3e6119d7.chunk.js.map
