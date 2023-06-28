4d798aefc33b0ae465d4fe9548724bb84d623e1e
Van-Giang NGUYEN <118123724+Amurion97@users.noreply.github.com>
2023-06-28 23:12:23 +0700

    fix env var react_app_

--- src/features/customAPI.js
+++ src/features/customAPI.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const customAPIv1 = () => {
-    console.log("process.env.BE:", process.env.BE)
+    console.log("process.env.REACT_APP_BE:", process.env.REACT_APP_BE);
     return axios.create({
-        baseURL: process.env.BE ? process.env.BE : 'http://127.0.0.1:5000/v1/',
+        baseURL: process.env.REACT_APP_BE ? process.env.REACT_APP_BE : 'http://127.0.0.1:5000/v1/',
         headers: {
             Authorization: `Bearer ${(localStorage.getItem("user")) ? JSON.parse(localStorage.getItem("user")).token : ""}`
         }
