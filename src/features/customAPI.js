1b65f4338500f953fbae5abbfba05ea6711d75d9
Van-Giang NGUYEN <118123724+Amurion97@users.noreply.github.com>
2023-06-28 22:55:36 +0700

    log env variables

--- src/features/customAPI.js
+++ src/features/customAPI.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-export const customAPIv1 = () => axios.create({
-    baseURL: process.env.BE || 'http://127.0.0.1:5000/v1/',
-    headers: {
-        Authorization: `Bearer ${(localStorage.getItem("user")) ? JSON.parse(localStorage.getItem("user")).token : ""}`
-    }
-})
+export const customAPIv1 = () => {
+    console.log("process.env.BE:", process.env.BE)
+    return axios.create({
+        baseURL: process.env.BE ? process.env.BE : 'http://127.0.0.1:5000/v1/',
+        headers: {
+            Authorization: `Bearer ${(localStorage.getItem("user")) ? JSON.parse(localStorage.getItem("user")).token : ""}`
+        }
+    })
+}
 
