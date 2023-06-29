2f19218863edfb9a6ea79d3b7442e45bb1fbc305
Your Name <you@example.com>
2023-06-29 15:46:45 +0700

    chuyen sang quiSzearch

--- src/components/Question/InTestTaking/QuestionInAction.js
+++ src/components/Question/InTestTaking/QuestionInAction.js
@@ -1,19 +1,19 @@
-import { Box, Button, Grid, Paper, Radio, Stack, Typography } from "@mui/material";
+import {Box, Button, Grid, Paper, Radio, Stack, Typography} from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
-import { alpha, useTheme } from "@mui/material/styles";
+import {alpha, useTheme} from "@mui/material/styles";
 
 const BG_COLOR = ["#2BA687", "#1976D2", "#F0A001", "#F200BE", "#CD1E3F"];
 
 export function QuestionInAction({
-    currentQuestion,
-    handleAnswerClick,
-    currentAnswerList,
-    handleNextQuestion,
-    handlePreviousQuestion,
-    submitForm,
-    totalQuestion,
-    currentQuestionIndex
-}) {
+                                     currentQuestion,
+                                     handleAnswerClick,
+                                     currentAnswerList,
+                                     handleNextQuestion,
+                                     handlePreviousQuestion,
+                                     submitForm,
+                                     totalQuestion,
+                                     currentQuestionIndex
+                                 }) {
     const theme = useTheme()
     return (<>
         <Paper
@@ -23,7 +23,8 @@ export function QuestionInAction({
             }}
             elevation={3}
         >
-            <Grid container
+            <Grid
+                container
                 sx={{
                     p: 3,
                     height: '100%',
@@ -46,81 +47,77 @@ export function QuestionInAction({
                         </Typography>
                     </Paper>
                 </Grid>
-                <Grid item xs={12}>
-                    <Grid container>
-                        {currentQuestion.answers && currentQuestion.answers.map((answer, index) => (
-                            <Grid
-                                key={index}
-                                item xs={12 / currentQuestion.answers.length}
-                                sx={{
-                                    pl: (index === 0) ? 0 : 2,
-                                    height: '50%',
-                                }}
-                            >
-                                <Paper
-                                    sx={{
-                                        height: '100%',
-                                        bgcolor: BG_COLOR[index],
-                                        p: 2,
-                                        color: theme.palette.primary.contrastText,
-                                        boxShadow: `5px 8px ${alpha('#595959', 0.4)}`,
-                                    }}
-                                    elevation={3}
-                                >
+                {currentQuestion.answers && currentQuestion.answers.map((answer, index) => (
+                    <Grid
+                        key={index}
+                        item xs={12 / currentQuestion.answers.length}
+                        sx={{
+                            pl: (index === 0) ? 0 : 2,
+                            height: '50%',
+                        }}
+                    >
+                        <Paper
+                            sx={{
+                                height: '100%',
+                                bgcolor: BG_COLOR[index],
+                                p: 2,
+                                color: theme.palette.primary.contrastText,
+                                boxShadow: `5px 8px ${alpha('#595959', 0.4)}`,
+                            }}
+                            elevation={3}
+                        >
 
-                                    <Box sx={{
-                                        width: '100%',
-                                        position: 'relative',
-                                    }}>
-                                        <Paper sx={{
-                                            borderRadius: `${currentQuestion.type.id <= 2 ? "50%" : theme.shape.borderRadius}`,
-                                            mr: 1,
-                                            mt: 0.5,
-                                            mb: 1,
-                                            height: '48px',
-                                            width: '48px',
-                                            position: 'absolute',
-                                            right: 0,
-                                            textAlign: 'center'
-                                        }}>
-                                            {currentQuestion.type.id <= 2 ?
-                                                <Radio
-                                                    onClick={() => {
-                                                        handleAnswerClick(answer.id)
-                                                    }
-                                                    }
-                                                    checked={currentAnswerList === answer.id}
-                                                    sx={{
-                                                        height: '48px',
-                                                        width: '48px',
-                                                    }}
-                                                />
-                                                :
-                                                <Checkbox
-                                                    checked={currentAnswerList.includes(answer.id)}
-                                                    color="success"
-                                                    onClick={() => {
-                                                        handleAnswerClick(answer.id)
-                                                    }}
-                                                    sx={{
-                                                        height: '48px',
-                                                        width: '48px',
-                                                    }}
-                                                />
+                            <Box sx={{
+                                width: '100%',
+                                position: 'relative',
+                            }}>
+                                <Paper sx={{
+                                    borderRadius: `${currentQuestion.type.id <= 2 ? "50%" : theme.shape.borderRadius}`,
+                                    mr: 1,
+                                    mt: 0.5,
+                                    mb: 1,
+                                    height: '48px',
+                                    width: '48px',
+                                    position: 'absolute',
+                                    right: 0,
+                                    textAlign: 'center'
+                                }}>
+                                    {currentQuestion.type.id <= 2 ?
+                                        <Radio
+                                            onClick={() => {
+                                                handleAnswerClick(answer.id)
                                             }
-                                        </Paper>
-                                    </Box>
-
-                                    <Typography variant={'h5'}
-                                        sx={{ mt: 8 }}>
-                                        {answer.content}
-                                    </Typography>
-
+                                            }
+                                            checked={currentAnswerList === answer.id}
+                                            sx={{
+                                                height: '48px',
+                                                width: '48px',
+                                            }}
+                                        />
+                                        :
+                                        <Checkbox
+                                            checked={currentAnswerList.includes(answer.id)}
+                                            color="success"
+                                            onClick={() => {
+                                                handleAnswerClick(answer.id)
+                                            }}
+                                            sx={{
+                                                height: '48px',
+                                                width: '48px',
+                                            }}
+                                        />
+                                    }
                                 </Paper>
-                            </Grid>
-                        ))}
+                            </Box>
+
+                            <Typography variant={'h5'}
+                                        sx={{mt: 8}}>
+                                {answer.content}
+                            </Typography>
+
+                        </Paper>
                     </Grid>
-                </Grid>
+                ))}
             </Grid>
         </Paper>
         <Stack
@@ -135,25 +132,25 @@ export function QuestionInAction({
         >
             {(handlePreviousQuestion && currentQuestionIndex > 0) &&
                 <Button variant="outlined" size='large'
-                    sx={{
-                        fontSize: 20,
-                    }}
-                    onClick={handlePreviousQuestion}
+                        sx={{
+                            fontSize: 20,
+                        }}
+                        onClick={handlePreviousQuestion}
                 >
                     Previous
                 </Button>}
             {currentQuestionIndex === totalQuestion - 1 ?
-                <Button variant="contained" size='large' sx={{ fontSize: 20, }}
-                    onClick={() => {
-                        submitForm()
-                        handleNextQuestion()
-                    }}
+                <Button variant="contained" size='large' sx={{fontSize: 20,}}
+                        onClick={() => {
+                            submitForm()
+                            handleNextQuestion()
+                        }}
                 >
                     Submit
                 </Button>
                 :
-                <Button variant="contained" size='large' sx={{ fontSize: 20, }}
-                    onClick={handleNextQuestion}
+                <Button variant="contained" size='large' sx={{fontSize: 20,}}
+                        onClick={handleNextQuestion}
                 >
                     Next
                 </Button>}
