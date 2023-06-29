8d0500c16c10116e35adbe9dc74284c77c52b12c
Van-Giang NGUYEN <118123724+Amurion97@users.noreply.github.com>
2023-06-29 14:02:08 +0700

    fix host socket

--- src/app/socket.js
+++ src/app/socket.js
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BE : 'http://localhost:5000';
+const URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BE_SOCKET : 'http://localhost:5000';
 
 export const socket = io(URL, {
     autoConnect: false
