92db6659862edb0fadf602b843c3427605099043
duylo <duyvu06497@gmail.com>
2023-06-30 09:13:14 +0700

    da responsive GroupWaitingRoomStudentLounge

--- src/pages/Student/GroupTest/GroupWaitingRoom.js
+++ src/pages/Student/GroupTest/GroupWaitingRoom.js
@@ -1,170 +1,155 @@
-import {Avatar, Box, Card, CardContent, Divider, Grid, MenuList, Paper, Typography} from "@mui/material";
+import {
+    Avatar,
+    Box,
+    Card,
+    CardContent,
+    Grid,
+    Typography,
+    useMediaQuery,
+} from "@mui/material";
 import CardHeader from "@mui/material/CardHeader";
+import GroupsTwoToneIcon from "@mui/icons-material/GroupsTwoTone";
+import { useTheme } from "@mui/material/styles";
+// MUI----------------------------------------------------------------
 
-//icon mui
-import GroupsTwoToneIcon from '@mui/icons-material/GroupsTwoTone';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+
+// React----------------------------------------------------------------
+
 import StudentsLounge from "./StudentsLounge";
-import {useSelector} from "react-redux";
-import {selectUser} from "../../../features/user/userSlice";
-import {useLocation, useNavigate} from "react-router-dom";
-import {useTheme} from "@mui/material/styles";
-import {socket} from "../../../app/socket";
 
+//Page  ----------------------------------------------------------------
+
+import { socket } from "../../../app/socket";
+// Socket----------------------------------------------------------------
+
+// Feature----------------------------------------------------------------
+import { selectUser } from "../../../features/user/userSlice";
 
 export default function GroupWaitingRoom() {
-    // const [students, setStudents] = useState([])
-    const theme = useTheme()
-    const navigate = useNavigate()
+    const theme = useTheme();
+    const navigate = useNavigate();
+    const isMd = useMediaQuery(theme.breakpoints.up("md"));
 
-    const url = new URL(window.location.href)
+    const url = new URL(window.location.href);
     const searchParams = new URLSearchParams(url.search);
     const roomCode = searchParams.get("code");
     console.log("roomCode:", roomCode);
 
-    const user = useSelector(selectUser)
+    const user = useSelector(selectUser);
     const location = useLocation();
     console.log("location in student lobby:", location);
-    const {state} = location;
+    const { state } = location;
 
     const [peopleList, setPeopleList] = useState(state ? state.peopleList : []);
-    // setPeopleList()
     console.log("peopleList:", peopleList);
 
-    const [isStartingTest, setIsStartingTest] = useState(false);
-
     useEffect(() => {
-        if (!state) {
-            socket.connect();
-        }
-
-        console.log('this [] effect on student lobby is running')
+        socket.connect();
+        console.log("this effect is running");
 
         function onConnect() {
-            socket.emit('join-lobby',
-                {roomCode: roomCode, email: user.info.email},
+            socket.emit(
+                "join-lobby",
+                { roomCode: roomCode, email: user.info.email },
                 (res) => {
-                    console.log('join-lobby', res);
+                    console.log("join-lobby", res);
                     if (res.success !== false) {
                         setPeopleList(res);
                     }
-
-                })
+                }
+            );
         }
 
-
         function onLobbyUpdate(arg) {
-            console.log('lobby-update:', arg);
+            console.log("lobby-update:", arg);
             if (arg.join) {
-                setPeopleList((list) => [...list, arg.person])
+                setPeopleList((list) => [...list, arg.person]);
             } else if (arg.leave) {
-                setPeopleList((list) => list.filter(item => item.email !== arg.email))
+                setPeopleList((list) =>
+                    list.filter((item) => item.email !== arg.email)
+                );
             }
         }
 
         function onStartTest(arg) {
-            console.log('start-test:', arg);
-            setIsStartingTest(true);
-            navigate('/groupTestTaking', {
+            console.log("start-test:", arg);
+            navigate("/groupTestTaking", {
                 state: {
                     test: arg.test,
-                    roomCode: roomCode
-                }
-            })
+                    roomCode: roomCode,
+                },
+            });
         }
-        if (!state) {
-            socket.on('connect', onConnect);
-        }
-
-        socket.on('lobby-update', onLobbyUpdate);
-        socket.on('start-test', onStartTest);
-
+        socket.on("connect", onConnect);
+        socket.on("lobby-update", onLobbyUpdate);
+        socket.on("start-test", onStartTest);
 
         return () => {
-            console.log('return for this [] effect on student lobby is running')
+            socket.off("connect", onConnect);
+            socket.off("lobby-update", onLobbyUpdate);
+            socket.off("start-test", onStartTest);
+            socket.disconnect();
+        };
+    }, []);
 
-            socket.off('connect', onConnect);
-            socket.off('lobby-update', onLobbyUpdate);
-            socket.off('start-test', onStartTest);
-
-        }
-
-    }, [])
-
-    useEffect(()=> {
-
-        return () => {
-            console.log('return for this [isStartingTest] effect on student lobby is running')
-            console.log("isStartingTest:", isStartingTest);
-
-            if (!isStartingTest) {
-
-                socket.disconnect();
-            }
-        }
-    }, [isStartingTest])
     return (
         <>
             <Box
                 sx={{
                     p: 3,
-
-                    margin: 'auto',
-                    // transform: "scale(1.2)"
-                    borderWidth: '0px',
-                    [theme.breakpoints.up('md')]: {
-                        // backgroundColor: theme.palette.primary.main,
-                        maxWidth: '1200px',
+                    margin: "auto",
+                    borderWidth: "0px",
+                    [theme.breakpoints.up("md")]: {
+                        maxWidth: "1200px",
                         p: 10,
                     },
-                }}>
-
-                <Grid container
-                    // spacing={3}
-                      sx={{
-                          pb: 3,
-                      }}
-                >
-                    <Grid item xs={8} sx={{pr: 3}}>
-                        <Card sx={{
-                            // pb: "24px",
-                            height: "100%"
-                        }}>
+                    maxWidth: "100%", // Thêm thuộc tính maxWidth để không bị quá to trên màn hình nhỏ
+                }}
+            >
+                <Grid container spacing={3}>
+                    <Grid item xs={12} md={8} sx={{ pr: isMd ? 3 : 0 }}>
+                        <Card
+                            sx={{
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "space-between",
+                            }}
+                        >
                             <CardHeader
                                 avatar={
                                     <Avatar
                                         src="/assets/images/avatars/avatar_default.jpg"
                                         alt="photoURL"
-                                        sx={{height: '80px', width: '80px'}}
-                                    />}
+                                        sx={{ height: "80px", width: "80px" }}
+                                    />
+                                }
                                 title={user.info.email}
                                 subheader="You"
                                 titleTypographyProps={{
-                                    variant: 'h4'
+                                    variant: "h4",
                                 }}
                                 subheaderTypographyProps={{
-                                    variant: 'h5'
+                                    variant: "h5",
                                 }}
                                 sx={{
-                                    p: 5
+                                    p: 5,
                                 }}
                             />
                         </Card>
                     </Grid>
 
-                    <Grid item xs={4}>
-                        <Card
-                            sx={{
-                                // display: "flex",
-                                // justifyContent: "center",
-                                // alignItems: "center",
-                                height: "100%",
-                                textAlign: 'center',
-                            }}>
-                            <CardContent
-                                sx={{pt: 5}}
-                            >
-                                <Typography variant="h4" color="text.secondary" gutterBottom>
+                    <Grid item xs={12} md={4}>
+                        <Card sx={{ height: "100%", textAlign: "center" }}>
+                            <CardContent sx={{ pt: 5 }}>
+                                <Typography
+                                    variant="h4"
+                                    color="text.secondary"
+                                    gutterBottom
+                                >
                                     JOIN CODE
                                 </Typography>
                                 <Typography variant="h3" component="div">
@@ -173,43 +158,62 @@ export default function GroupWaitingRoom() {
                             </CardContent>
                         </Card>
                     </Grid>
-
                 </Grid>
 
-                <Grid container
-                >
-
-                    <Grid item xs={10}
-                          sx={{
-                              display: "flex",
-                              alignItems: "center",
-                          }}>
+                <Grid container>
+                    <Grid
+                        item
+                        xs={12}
+                        md={10}
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            mt: isMd ? 0 : 3,
+                        }}
+                    >
                         <Typography variant="h4">
                             Waiting for the host to start....
                         </Typography>
                     </Grid>
-                    <Grid item xs={2}>
-                        <Card sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            height: "100%"
-                        }}>
-                            <Box sx={{
-                                display: "flex", alignItems: "center",
-                                py: 1
-                            }}>
-                                <GroupsTwoToneIcon fontSize='large'/>
-                                <Typography variant='h4' sx={{ml: "4px"}}>{peopleList.length}</Typography>
+                    <Grid item xs={12} md={2}  sx={{
+                        mt:1
+                    }}>
+                        <Card
+                            sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                height: "100%",
+                            
+                                
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    py: 1,
+                                    
+                                }}
+                            >
+                                <GroupsTwoToneIcon fontSize="large" />
+                                
+                                    <Typography variant="h4" sx={{ ml: "4px" }}>
+                                        {peopleList.length}
+                                    </Typography>
+                                
                             </Box>
                         </Card>
                     </Grid>
-
                 </Grid>
-
             </Box>
 
-            <StudentsLounge peopleList={peopleList.filter(item => item.email !== user.info.email)}/>
+            <StudentsLounge
+                peopleList={peopleList.filter(
+                    (item) => item.email !== user.info.email
+                )}
+            />
         </>
-    )
+    );
 }
--- src/pages/Student/GroupTest/StudentsLounge.js
+++ src/pages/Student/GroupTest/StudentsLounge.js
@@ -1,28 +1,27 @@
-import {Avatar, Box, Card, Grid} from "@mui/material";
+import { Avatar, Box, Card, Grid } from "@mui/material";
 import CardHeader from "@mui/material/CardHeader";
 
-export default function StudentsLounge({peopleList}) {
-    return (
-        <>
-            <Grid container spacing={3} sx={{
-                pt: 5,
-                px: 5
-            }}>
-                {peopleList.map((person) =>
-                    (<Grid key={person.id} item xs={4}>
-
-                        <Card sx={{pb: "24px", height: "100%"}}>
-                            <CardHeader
-                                avatar={<Avatar src="/assets/images/avatars/avatar_default.jpg"
-                                                alt="photoURL"/>}
-                                title={person.email}
-                                subheader="Student"
-                            />
-                        </Card>
-
-                    </Grid>))}
-
-            </Grid>
-        </>
-    )
+export default function StudentsLounge({ peopleList }) {
+  return (
+    <>
+      <Grid container spacing={3} sx={{ pt: 5, px: 2 }}>
+        {peopleList.map((person) => (
+          <Grid key={person.id} item xs={12} sm={6} md={4}>
+            <Card sx={{ pb: "24px", height: "100%" }}>
+              <CardHeader
+                avatar={
+                  <Avatar
+                    src="/assets/images/avatars/avatar_default.jpg"
+                    alt="photoURL"
+                  />
+                }
+                title={person.email}
+                subheader="Student"
+              />
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
 }
